02eb43c55ac1f178161b61dc82854ada368edf23
andrew362 <37003497+andrew362@users.noreply.github.com>
2018-05-08 21:27:12 +0200

    section4

--- script/script.js
+++ script/script.js
@@ -1,21 +1,79 @@
-var navBg = $('#nav-main');
+   $(document).ready(function () {
+
+       var navBg = $('#nav-main');
 
 
-// animacja danych kontaktowych w nawigacji
+       // animacja danych kontaktowych w nawigacji
 
-$(window).on('scroll', function(){
-    console.log($(this).scrollTop());
-    var navContactInfo = $('#nav-contact-info');
-    if($(this).scrollTop() > 50){
-        //navContactInfo.stop();
-        navContactInfo.slideUp(200,function() {
-            navContactInfo.removeClass('d-md-block');
-            navBg.css('backgroundColor', 'rgba( 0, 0, 0, 0.6 )');
-        });
-        
-    } else {
-            navContactInfo.addClass('d-md-block');
-            navContactInfo.slideDown(200);
-            navBg.css('backgroundColor', 'rgba( 0, 0, 0, 0)');
+       $(window).on('scroll', function () {
+           console.log($(this).scrollTop());
+           var navContactInfo = $('#nav-contact-info');
+           if ($(this).scrollTop() > 50) {
+               //navContactInfo.stop();
+               navContactInfo.slideUp(200, function () {
+                   navContactInfo.removeClass('d-md-block');
+                   navBg.css('backgroundColor', 'rgba( 0, 0, 0, 0.6 )');
+               });
+
+           } else {
+               navContactInfo.addClass('d-md-block');
+               navContactInfo.slideDown(200);
+               navBg.css('backgroundColor', 'rgba( 0, 0, 0, 0)');
+           }
+       });
+
+
+
+
+       $('.slider-for').slick({
+           slidesToShow: 1,
+           slidesToScroll: 1,
+           fade: true,
+           appendArrows: $('.slider-for '),
+           arrows: true,
+           asNavFor: '.slider-nav'
+       });
+       
+       $('.slider-nav').slick({
+           slidesToShow: 7,
+           slidesToScroll: 1,
+           arrows: false,
+           asNavFor: '.slider-for',
+           centerMode: true,
+           focusOnSelect: true,
+           autoplay: true,
+           autoplaySpeed: 2000,
+           dots: true,
+           responsive: [
+               {
+                   breakpoint: 992,
+                   settings: {
+                       slidesToShow: 5,
+                       slidesToScroll: 5,
+                       infinite: true,
+                       dots: true
+                   }
+    },
+               {
+                   breakpoint: 767,
+                   settings: {
+                       slidesToShow: 3,
+                       slidesToScroll: 3,
+                       dots: true
+
+                   }
+    },
+               {
+                   breakpoint: 450,
+                   settings: {
+                       slidesToShow: 2,
+                       slidesToScroll: 2,
+                       dots: true
+                   }
     }
-});
+    ]
+       });
+
+
+
+   });
